7122f50f5a130d63a081d0c026de3617386e86a0
Victoria Kee <victoriakee95@gmail.com>
2020-06-11 15:02:32 -0500

    Create add function

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ function get_todos() {
     todos = JSON.parse(todos_str); //used to parse the JSON data back to JavaScript so it can be used
   }
   return todos;
+};
+
+function add() {
+  const task = document.getElementById('task').value; //retrieve input value of task
+
+  let todos = get_todos(); //retrieve pre-existing items
+  todos.push(task); //append new item
+  localStorage.setItem('todo', JSON.stringify(todos)); //save returned strings to localStorage (setItem)
+
+  show(); //implement show function
+
+  return false; //avoid further action of click event
 }
